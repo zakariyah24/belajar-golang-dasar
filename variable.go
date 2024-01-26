e926eb1d197effc32ca6c3ca7abbdab8e9abaf0d
zaka <fzakariyah1@gmail.com>
2024-01-26 23:47:42 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Zakariyah Firmansyah"
+	fmt.Println(name)
+
+	name = "Zakariyah Pantouw"
+	fmt.Println(name)
+
+	var (
+		firstName  = "Zakariyah"
+		middleName = "Firmansyah"
+		lastName   = "Pantouw"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
+}
