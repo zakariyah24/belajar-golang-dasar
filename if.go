6573da947c2e6293601bebe648df33f6d0d7c4c6
zaka <fzakariyah1@gmail.com>
2024-01-27 23:47:09 +0700

    pengandaian

---
+++ if.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Firmansyah"
+
+	if name == "Zakariyah" {
+		fmt.Println("Hello Zakariyah")
+	} else if name == "Budi" {
+		fmt.Println("Hello Budi")
+	} else if name == "Joko" {
+		fmt.Println("Hello Joko")
+	} else {
+		fmt.Println("Hi, Boleh Kenalan?")
+	}
+
+	if length := len(name); length > 5 {
+		fmt.Println("Nama terlalu panjang")
+	} else {
+		fmt.Println("Nama sudah benar")
+	}
+}
