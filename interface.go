1d07c828cc9ac2b80a5487259db8875f950592e5
zaka <fzakariyah1@gmail.com>
2024-03-06 22:28:57 +0700

    interface

---
+++ interface.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(value HasName) {
+	fmt.Println("Hello ", value.GetName())
+
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+
+}
+
+type Animal struct {
+	Name string
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+	person := Person{Name: "Zaka"}
+	SayHello(person)
+
+	animal := Animal{"Kucing"}
+	SayHello(animal)
+}
